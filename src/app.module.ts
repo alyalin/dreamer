9f7a984e617c4c7be8bf7ee6c7719a52faf72e24
Alexander Lyalin <occvlt@yandex.ru>
2020-05-21 23:25:31 +0400

    :muscle: add auth module & add orm connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import * as ormConnection from "./ormconfig";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => (ormConnection),
+    }),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
