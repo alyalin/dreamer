d86802ed80a0e518f1acd172d82d1525365a29b2
Alexander Lyalin <occvlt@yandex.ru>
2020-05-21 23:14:02 +0400

    :muscle: add ormconfig

---
+++ src/ormconfig.ts
@@ -0,0 +1,35 @@
+import { ConnectionOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { config } from 'dotenv';
+import { APP_ENV } from './constants';
+
+config();
+
+const ormConnection: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+  // We are using migrations, synchronize should be set to false.
+  synchronize: false,
+  // Run migrations automatically,
+  // you can disable this if you prefer running migration manually.
+  migrationsRun: process.env.NODE_ENV !== APP_ENV.PRODUCTION,
+  namingStrategy: new SnakeNamingStrategy(),
+  logging: process.env.NODE_ENV === APP_ENV.DEVELOPMENT,
+  // logger: 'file',
+  // Allow both start:prod and start:dev to use migrations
+  // __dirname is either dist or src folder, meaning either
+  // the compiled js in prod or the ts in dev.
+  migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
+  cli: {
+    // Location of migration should be inside src folder
+    // to be compiled into dist/ folder.
+    migrationsDir: 'src/migrations',
+  },
+};
+
+module.exports = ormConnection;
