c2b751811443bb8265499e1af2eeb5652c32fc20
Alexander Lyalin <occvlt@yandex.ru>
2020-05-23 16:56:47 +0400

    :eyes: rewrite serializer

--- src/modules/auth/serializer/session.serializer.ts
+++ src/modules/auth/serializer/session.serializer.ts
@@ -1,19 +1,30 @@
-import {PassportSerializer} from "@nestjs/passport";
-import {Injectable} from "@nestjs/common";
+import { PassportSerializer } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
 import { SessionPayload } from '../interfaces/session-payload.interface';
-
+import { UserService } from '../../user/user.service';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  constructor() {
+  constructor(private userService: UserService) {
     super();
   }
 
-  serializeUser(user: SessionPayload, done: (err: Error | null, user: SessionPayload) => void): void {
-    done(null, user);
+  serializeUser(
+    user: SessionPayload,
+    done: (err: Error | null, userId: string) => void,
+  ): void {
+    done(null, user.id);
   }
 
-  deserializeUser(user: SessionPayload, done: (err: Error | null, payload?: SessionPayload) => void): void {
-    done(null, user);
+  deserializeUser(
+    userId: string,
+    done: (err: Error | null, payload?: SessionPayload) => void,
+  ): void {
+    this.userService
+      .findById(userId)
+      .then(user => {
+        return done(null, user.toSessionSerializer());
+      })
+      .catch(error => done(error));
   }
 }
