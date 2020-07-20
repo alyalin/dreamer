0f951eb5bdcb0c7fb8ec048be1b0ed2ab8e4abd4
alyalin <occvlt@yandex.ru>
2020-07-20 10:13:33 +0400

    :eyes: edit dto for reset password

--- src/modules/auth/dto/reset-password.dto.ts
+++ src/modules/auth/dto/reset-password.dto.ts
@@ -1,17 +1,6 @@
-import { Match } from '../../../common/decorators/match.decorator';
-import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
-import { Errors } from '../../../constants';
+import { IsEmail } from 'class-validator';
 
 export class ResetPasswordDto {
-  @IsString()
-  @MinLength(8)
-  @MaxLength(20)
-  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {message: Errors.WEAK_PASSWORD_ERROR})
-  password: string;
-
-  @IsString()
-  @MinLength(8)
-  @MaxLength(20)
-  @Match('password')
-  confirmPassword: string;
+  @IsEmail()
+  email: string;
 }
